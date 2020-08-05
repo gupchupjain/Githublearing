40494d9f76f7372c4a6e77b2c703957a1fe52858
Nalin Porwal <nalin.porwal@advantal.net>
2020-08-05 23:41:31 +0530

    TableView Data Source

--- Githublearing/ViewController.swift
+++ Githublearing/ViewController.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
