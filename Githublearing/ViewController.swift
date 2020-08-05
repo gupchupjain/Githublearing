0bd5bc906287c9ae0a5e81c65627e5b20327bcea
Nalin Porwal <nalin.porwal@advantal.net>
2020-08-05 23:55:56 +0530

    guy

--- Githublearing/ViewController.swift
+++ Githublearing/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource,UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        11
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
