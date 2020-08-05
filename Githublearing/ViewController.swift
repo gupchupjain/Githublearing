0c235b2afca83f66c150f8656f35bed3f25ca915
Nalin Porwal <nalin.porwal@advantal.net>
2020-08-05 23:32:39 +0530

    nalin1

--- Githublearing/ViewController.swift
+++ Githublearing/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("AdvNalin Porwal")
+        print("AdvNalin 1")
         // Do any additional setup after loading the view.
     }
 
